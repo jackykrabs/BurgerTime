3f0106b3942071befffa3a66b7cb60637dbf09b2
wil collier <ww.wc48@gmail.com>
2017-03-08 10:22:38 -0600

    some new classes

---
+++ BurgerTime/Enemy.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Enemy.h"
+
+
+Enemy::Enemy() : GameObject("hotdog")
+{
+
+}
+
+
+Enemy::~Enemy()
+{
+}
---
+++ BurgerTime/PepperShot.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "PepperShot.h"
+
+
+PepperShot::PepperShot()
+{
+}
+
+
+PepperShot::~PepperShot()
+{
+}
